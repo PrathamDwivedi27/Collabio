dd04930007704717b383031e7fd011731cfd3438
PrathamDwivedi27 <prathamd549@gmail.com>
2024-11-24 02:19:11 +0530

    [Feat]: Completed the toolbar section

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,4 +1,16 @@
+import { Actions } from '@/components/actions'
+import { Hint } from '@/components/hint'
+import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
+import { api } from '@/convex/_generated/api'
+import { Id } from '@/convex/_generated/dataModel'
+import { cn } from '@/lib/utils'
+import { useRenameModal } from '@/store/use-rename-modal'
+import { useQuery } from 'convex/react'
+import { Menu } from 'lucide-react'
+import { Poppins } from 'next/font/google'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 
@@ -6,10 +18,69 @@ interface InfoProps {
   boardId?: string
 }
 
+const font=Poppins({
+  subsets:['latin'],
+  weight:['600'],
+})
+
+const TabSeparator=()=>{
+  return (
+    <div className='text-neutral-300 px-1.5'>
+      |
+    </div>
+  )
+}
+
 const Info = ({boardId}:InfoProps) => {
+  const {onOpen}=useRenameModal();
+  const data=useQuery(api.board.get,{
+    id:boardId as Id<"boards">,
+  })
+
+  if(!data) return <InfoSkeleton/>
+
+
+
   return (
     <div className='absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md'>
-      Todo info about board
+      <Hint label='Go to boards' side='bottom' sideOffset={10}>
+      <Button asChild className='px-2' variant='board'>
+        <Link href='/'>
+        <Image
+          src='/logo.svg'
+          alt='logo'
+          width={40}
+          height={40}
+        />
+        <span className={cn("font-semibold text-xl ml-2 text-black",font.className)}>
+          Board
+        </span>
+        </Link>
+      </Button>
+      </Hint>
+      <TabSeparator/>
+      <Hint label='Edit title' side='bottom' sideOffset={10}>
+      <Button variant="board" className='text-base font-normal px-2'
+        onClick={()=>onOpen(data._id,data.title)}
+      >
+        {data.title}
+      </Button>
+      </Hint>
+      <TabSeparator/>
+      <Actions
+        id={data._id}
+        title={data.title}
+        side='bottom'
+        sideOffset={10}
+      >
+        <div>
+          <Hint label='Main Menu' side='bottom' sideOffset={10}>
+              <Button size='icon' variant='board'>
+                <Menu/>
+              </Button>
+          </Hint>
+        </div>
+      </Actions>
     </div>
   )
 }
