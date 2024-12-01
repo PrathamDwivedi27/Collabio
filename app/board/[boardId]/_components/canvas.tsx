36bdd42719899d4c416bcb5e7d2ce77ddb9ced0f
PrathamDwivedi27 <prathamd549@gmail.com>
2024-12-01 19:44:42 +0530

    [Feat]: Add the selection box

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -10,7 +10,7 @@ import { useHistory,
   useMutation } from '@liveblocks/react';
 import CursorsPresence from './cursors-presence';
 import { Camera } from '@/types/canvas';
-import { connectionIdToColor, pointerEventToCanvasPoint, resizeBounds } from '@/lib/utils';
+import { connectionIdToColor, findIntersectionLayersWithRectangle, pointerEventToCanvasPoint, resizeBounds } from '@/lib/utils';
 import { useOthersMapped, useStorage } from '@liveblocks/react/suspense';
 import {nanoid} from 'nanoid';
 import { LiveObject } from '@liveblocks/client';
@@ -118,6 +118,34 @@ const Canvas = ({ boardId }: CanvasProps) => {
       }
     },[]);
 
+    const updateSelectionNet=useMutation (({storage,setMyPresence},current:Point,origin:Point)=>{
+      const layers=storage.get("layers").toImmutable();
+      setCanvasState({
+        mode:CanvasMode.SelectionNet,
+        origin,
+        current,
+      });
+
+      const ids=findIntersectionLayersWithRectangle(
+        layerIds,
+        layers,
+        origin,
+        current
+      )
+      setMyPresence({selection:ids});
+      
+    },[layerIds])
+
+    const startMultiSelection=useCallback((current:Point,origin:Point)=>{
+      if(Math.abs(current.x-origin.x)+Math.abs(current.y-origin.y)>3){
+        setCanvasState({
+          mode:CanvasMode.SelectionNet,
+          origin,
+          current
+        });
+      }
+    },[]);
+
    const resizeSelectedLayer=useMutation((
     {storage,self},point:Point
    )=>{
@@ -169,7 +197,15 @@ const Canvas = ({ boardId }: CanvasProps) => {
 
       const current=pointerEventToCanvasPoint(e,camera);
 
-      if(canvasState.mode===CanvasMode.Translating){
+      if(canvasState.mode===CanvasMode.Pressing){
+        startMultiSelection(current,canvasState.origin);
+      }
+
+      else if(canvasState.mode===CanvasMode.SelectionNet){
+        updateSelectionNet(current,canvasState.origin);
+      }
+
+      else if(canvasState.mode===CanvasMode.Translating){
         // console.log("translating");
         translateSelectedLayer(current);
       }
@@ -191,7 +227,7 @@ const Canvas = ({ boardId }: CanvasProps) => {
       });
     },[]);
 
-    const onPointerDown=useCallback(()=>{
+    const onPointerDown=useCallback((e:React.PointerEvent)=>{
       const point=pointerEventToCanvasPoint(e,camera);
 
       if(canvasState.mode===CanvasMode.Pressing){
@@ -315,6 +351,16 @@ const Canvas = ({ boardId }: CanvasProps) => {
           <SelectionBox
             onResizeHandlePointer={onResizeHandlePointerDown}
           />
+          {canvasState.mode===CanvasMode.SelectionNet && canvasState.current!=null && (
+            <rect
+              className='fill-blue-500/5 stroke-blue-500 stroke-1 '
+              x={Math.min(canvasState.origin.x,canvasState.current.x)}
+              y={Math.min(canvasState.origin.y,canvasState.current.y)}
+              width={Math.abs(canvasState.origin.x-canvasState.current.x)}
+              height={Math.abs(canvasState.origin.y-canvasState.current.y)}
+            
+            />
+          )}
           <CursorsPresence />
         </g>
       </svg>
